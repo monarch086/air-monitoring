1d92dde9cb2a524692f9ab6508738d1f75b1e207
Oleksandr Barsuk <oleksandr.barsuk@acropolium.com>
2024-05-17 02:17:32 +0300

    Update DataProviding service

--- src/backend/AirMonitoring.DataProviding/Functions.cs
+++ src/backend/AirMonitoring.DataProviding/Functions.cs
@@ -1,8 +1,13 @@
-using System.Net;
-using Amazon.Lambda.Core;
+using AirMonitoring.Core.Extensions;
+using AirMonitoring.Core.HTTP;
+using AirMonitoring.Core.HttpResponses;
+using AirMonitoring.Core.Model;
+using AirMonitoring.Core.Persistence;
+using AirMonitoring.DataProviding.Model;
 using Amazon.Lambda.APIGatewayEvents;
-using Amazon.Lambda.Annotations;
-using Amazon.Lambda.Annotations.APIGateway;
+using Amazon.Lambda.Core;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -11,35 +16,49 @@ namespace AirMonitoring.DataProviding;
 
 public class Functions
 {
-    /// <summary>
-    /// Default constructor that Lambda will invoke.
-    /// </summary>
-    public Functions()
+    private Dictionary<MeasurementType, Func<Measurement, string?>> dataSelectors = new Dictionary<MeasurementType, Func<Measurement, string?>>()
     {
-    }
+        { MeasurementType.Temperature, m => m.Sht31?.Temperature },
+        { MeasurementType.Humidity, m => m.Sht31?.Humidity },
+        { MeasurementType.Pressure, m => m.Bmp085?.Pressure.ToString() }
+    };
 
-
-    /// <summary>
-    /// A Lambda function to respond to HTTP Get methods from API Gateway
-    /// </summary>
-    /// <remarks>
-    /// This uses the <see href="https://github.com/aws/aws-lambda-dotnet/blob/master/Libraries/src/Amazon.Lambda.Annotations/README.md">Lambda Annotations</see> 
-    /// programming model to bridge the gap between the Lambda programming model and a more idiomatic .NET model.
-    /// 
-    /// This automatically handles reading parameters from an APIGatewayProxyRequest
-    /// as well as syncing the function definitions to serverless.template each time you build.
-    /// 
-    /// If you do not wish to use this model and need to manipulate the API Gateway 
-    /// objects directly, see the accompanying Readme.md for instructions.
-    /// </remarks>
-    /// <param name="context">Information about the invocation, function, and execution environment</param>
-    /// <returns>The response as an implicit <see cref="APIGatewayProxyResponse"/></returns>
-    [LambdaFunction(Policies = "AWSLambdaBasicExecutionRole", MemorySize = 256, Timeout = 30)]
-    [RestApi(LambdaHttpMethod.Get, "/")]
-    public IHttpResult Get(ILambdaContext context)
+    public async Task<APIGatewayProxyResponse> FunctionHandler(JsonObject input, ILambdaContext context)
     {
-        context.Logger.LogInformation("Handling the 'Get' Request");
+        try
+        {
+            var query = input["queryStringParameters"].Deserialize<QueryModel>();
+            if (query == null) { return new BadRequestResponse(); }
 
-        return HttpResults.Ok("Hello AWS Serverless");
+            var repository = new MeasurementsRepo(context.Logger);
+            var deviceId = "S4D-12";
+
+            var range = TimeSpan.FromHours(10);
+            var from = DateTime.UtcNow - range;
+            var till = DateTime.UtcNow;
+            var records = await repository.GetList(deviceId, from, till);
+            var measurements = records
+                .Select(r => r.ToMeasurement());
+
+            var filledMeasurements = measurements.FillAbsentMeasurements(from, till);
+
+            var selector = dataSelectors[query.Type];
+            if (selector == null)
+            {
+                return new FailResponse($"{query.Type} measurement type is not supported.");
+            }
+
+            var data = filledMeasurements
+                .Select(selector)
+                .Where(v => v != null)
+                .ToArray();
+
+            return new SuccessResponse(JsonSerializer.Serialize(data));
+        }
+        catch (Exception e)
+        {
+            context.Logger.LogError(e.ToString());
+            return new FailResponse(e.ToString());
+        }
     }
 }
---
+++ src/backend/AirMonitoring.DataProviding/Model/QueryModel.cs
@@ -0,0 +1,9 @@
+﻿using AirMonitoring.Core.Model;
+
+namespace AirMonitoring.DataProviding.Model
+{
+    public class QueryModel
+    {
+        public MeasurementType Type { get; set; }
+    }
+}
