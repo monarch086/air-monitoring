11a2f612cbda69af050872cd83cb958267a10537
Oleksandr Barsuk <oleksandr.barsuk@acropolium.com>
2024-05-18 00:53:34 +0300

    Update build script - add all services

---
+++ src/backend/build.ps1
@@ -0,0 +1,51 @@
+Write-Output "Packaging services...";
+
+Set-Location ./AirMonitoring.DataProviding
+dotnet restore
+dotnet lambda package --configuration Release --framework net6.0 --output-package bin/Release/net6.0/deploy-package.zip
+Write-Output ">>> Finished packaging AirMonitoring.DataProviding";
+Set-Location ..
+
+Set-Location ./AirMonitoring.DataIngestion
+dotnet restore
+dotnet lambda package --configuration Release --framework net6.0 --output-package bin/Release/net6.0/deploy-package.zip
+Write-Output ">>> Finished packaging AirMonitoring.DataIngestion";
+Set-Location ..
+
+Set-Location ./AirMonitoring.DataValidation
+dotnet restore
+dotnet lambda package --configuration Release --framework net6.0 --output-package bin/Release/net6.0/deploy-package.zip
+Write-Output ">>> Finished packaging AirMonitoring.DataValidation";
+Set-Location ..
+
+Set-Location ./AirMonitoring.DataAnalysis
+dotnet restore
+dotnet lambda package --configuration Release --framework net6.0 --output-package bin/Release/net6.0/deploy-package.zip
+Write-Output ">>> Finished packaging AirMonitoring.DataAnalysis";
+Set-Location ..
+
+Set-Location ./AirMonitoring.Alerting
+dotnet restore
+dotnet lambda package --configuration Release --framework net6.0 --output-package bin/Release/net6.0/deploy-package.zip
+Write-Output ">>> Finished packaging AirMonitoring.Alerting";
+Set-Location ..
+
+Set-Location ./AirMonitoring.CommandProcessing
+dotnet restore
+dotnet lambda package --configuration Release --framework net6.0 --output-package bin/Release/net6.0/deploy-package.zip
+Write-Output ">>> Finished packaging AirMonitoring.CommandProcessing";
+Set-Location ..
+
+Set-Location ./AirMonitoring.MonthlyReporting
+dotnet restore
+dotnet lambda package --configuration Release --framework net6.0 --output-package bin/Release/net6.0/deploy-package.zip
+Write-Output ">>> Finished packaging AirMonitoring.MonthlyReporting";
+Set-Location ..
+
+Set-Location ./AirMonitoring.YearlyReporting
+dotnet restore
+dotnet lambda package --configuration Release --framework net6.0 --output-package bin/Release/net6.0/deploy-package.zip
+Write-Output ">>> Finished packaging AirMonitoring.YearlyReporting";
+Set-Location ..
+
+Write-Output ">>> >>> >>> Finished all services.";
