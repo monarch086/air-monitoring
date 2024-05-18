2eb908253ec59f0dc8971b76ee9770d3d4247fc9
Oleksandr Barsuk <oleksandr.barsuk@acropolium.com>
2024-05-18 16:14:29 +0300

    Add Bootstrap & Apexcharts

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
