c5719013700e0d7dc19f8bae40df9c5331ba2ec2
Oleksandr Barsuk <oleksandr.barsuk@acropolium.com>
2024-11-03 19:52:12 +0200

    Update latest-measurements lambda to be run by cron

--- src/backend/AirMonitoring.LatestMeasurements/Function.cs
+++ src/backend/AirMonitoring.LatestMeasurements/Function.cs
@@ -2,6 +2,7 @@ using AirMonitoring.Core.Config;
 using AirMonitoring.Core.Model.Events.SQS;
 using AirMonitoring.Core.Persistence;
 using AirMonitoring.Core;
+using AirMonitoring.Core.Extensions;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
 using System.Text.Json;
@@ -12,7 +13,7 @@ namespace AirMonitoring.LatestMeasurements;
 
 public class Function
 {
-    public async Task FunctionHandler(SQSEvent sqsEvent, ILambdaContext context)
+    public async Task FunctionHandler(object inputEvent, ILambdaContext context)
     {
         var config = await ConfigBuilder.Build(context.Logger);
         var bot = new ChatBot(config.Token);
@@ -20,18 +21,50 @@ public class Function
 
         try
         {
-            var payload = JsonSerializer.Deserialize<CommandEvent>(sqsEvent.Records[0].Body);
-            if (payload == null || payload.Command == null)
-                return;
+            var payload = inputEvent is SQSEvent sqsEvent && sqsEvent.Records.Count > 0
+                ? GetPayloadFromSqsEvent(sqsEvent, context)
+                : GetPayloadFromCronEvent();
 
-            context.Logger.LogInformation($"Received command: {payload.Command}.");
+            if (payload != null)
+            {
+                var range = TimeSpan.FromHours(10);
+                var from = DateTime.UtcNow - range;
+                var till = DateTime.UtcNow;
+                var records = await repository.GetList(payload.DeviceId, from, till);
+                var measurements = records
+                    .Select(r => r.ToMeasurement());
 
-            var lastMeasurement = (await repository.GetLatest(payload.DeviceId)).ToMeasurement();
-            await bot.Post(lastMeasurement.ToString(), payload.ChatId);
+                var filledMeasurements = measurements.FillAbsentMeasurements(from, till);
+
+                var tempData = filledMeasurements
+                    .Select(m => m.Sht31?.Temperature)
+                    .Where(v => v != null)
+                    .ToArray();
+
+                var chart = ChartGenerator.Generate(tempData, range, "t,°C");
+
+                var lastMeasurement = measurements.Last();
+                await bot.PostImageBytes(chart, lastMeasurement.ToString(), payload.ChatId);
+            }
         }
         catch (Exception e)
         {
             context.Logger.LogError(e.ToString());
         }
+    }
+
+    private CommandEvent? GetPayloadFromSqsEvent(SQSEvent sqsEvent, ILambdaContext context)
+    {
+        var payload = JsonSerializer.Deserialize<CommandEvent>(sqsEvent.Records[0].Body);
+        return payload;
+    }
+
+    private CommandEvent GetPayloadFromCronEvent()
+    {
+        return new CommandEvent
+        {
+            ChatId = 38627946,
+            DeviceId = "S4D-12"
+        };
     }
 }
